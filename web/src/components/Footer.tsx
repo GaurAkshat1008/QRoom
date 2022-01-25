79dcff9e6514bc52b5638f43fd8349e11aa6a87b
Akshat Gaur <76122113+jueviole-grace@users.noreply.github.com>
2022-01-25 20:39:26 +0530

    Delete web directory

--- web/src/components/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
-
-export const Footer = (props: FlexProps) => (
-  <Flex as="footer" py="2rem" {...props}    bgGradient="linear(to-l, #7928AD, #FF00AC)"
-  />
-)
