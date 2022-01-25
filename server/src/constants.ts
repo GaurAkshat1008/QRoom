5c73e2e5d944657f02b3790337bab331a159993a
Akshat Gaur <76122113+jueviole-grace@users.noreply.github.com>
2022-01-25 20:39:06 +0530

    Delete server directory

--- server/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const __prod__ = process.env.NODE_ENV === 'production'
-export const COOKIE_NAME = 'qid'
-export const CREATE_ROOM = 'new-room'
--- server/src/utils/auths.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from "type-graphql";
-
-@InputType()
-export class Auths {
-  @Field()
-  username!: string;
-  @Field()
-  password!: string;
-}
